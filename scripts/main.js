3e9dce9f4303bc93e56c2c7657bbcf43298de968
Muhammed-Haci <muhammed.hacilar@gmail.com>
2022-10-28 21:16:09 +0300

    u

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// =========== create overlay function =========== //
+function createOverlay() {
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
+}
+//# sourceMappingURL=main.js.map
